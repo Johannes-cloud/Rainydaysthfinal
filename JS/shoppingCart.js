37c070aeb3e676b774165206d7ca8423b2ab572b
Johannes-cloud <79262331+Johannes-cloud@users.noreply.github.com>
2022-07-03 22:36:48 +0200

    Add files via upload

---
+++ JS/shoppingCart.js
@@ -0,0 +1,164 @@
+const myButton1 = document.querySelector(".card_button1")
+const cart = document.querySelector(".cart")
+const cartTotal = document.querySelector(".cart-total")
+let total = 0;
+let count = 0;
+
+myButton1.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton2 = document.querySelector(".card_button2")
+
+myButton2.onclick = function(){
+    count++;
+    total += 100;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton3 = document.querySelector(".card_button3")
+
+myButton3.onclick = function(){
+    count++;
+    total += 150;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton4 = document.querySelector(".card_button4")
+
+myButton4.onclick = function(){
+    count++;
+    total += 200;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton5 = document.querySelector(".card_button5")
+
+myButton5.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton6 = document.querySelector(".card_button6")
+
+myButton6.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton7 = document.querySelector(".card_button7")
+
+myButton7.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton8 = document.querySelector(".card_button8")
+
+myButton8.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton9 = document.querySelector(".card_button9")
+
+myButton9.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton10 = document.querySelector(".card_button10")
+
+myButton10.onclick = function(){
+    count++;
+    total += 100;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton11 = document.querySelector(".card_button11")
+
+myButton11.onclick = function(){
+    count++;
+    total += 150;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton12 = document.querySelector(".card_button12")
+
+myButton12.onclick = function(){
+    count++;
+    total += 200;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton13 = document.querySelector(".card_button13")
+
+myButton13.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton14 = document.querySelector(".card_button14")
+
+myButton14.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton15 = document.querySelector(".card_button15")
+
+myButton15.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
+const myButton16 = document.querySelector(".card_button16")
+
+myButton16.onclick = function(){
+    count++;
+    total += 50;
+    cart.style.display = "block";
+    cartTotal.innerHTML = `Items: ${count} Total: $ ${total}`;
+    localStorage.setItem("total",total)
+}
+
